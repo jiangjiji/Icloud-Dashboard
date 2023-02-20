c40530904d3a990d2b076358d551932cbe1ee418
DESKTOP-GE78HP4\jiang <jiangliangiobit@gmail.com>
2023-02-20 14:20:42 +0800

    modify: icloud.com to icloud.com.cn

--- js/index.js
+++ js/index.js
@@ -1,25 +1,29 @@
-Array.from(document.getElementsByClassName("hover"))
-    .forEach(function(element){
-        element.addEventListener('click', function(e) {
-            if(e.currentTarget.id === "iCloud"){
-                window.open(`https://www.icloud.com`)
-            } else { window.open(`https://www.icloud.com/${e.currentTarget.id}`) }
-        })
-    });
+Array.from(document.getElementsByClassName("hover")).forEach(function (
+  element
+) {
+  element.addEventListener("click", function (e) {
+    if (e.currentTarget.id === "iCloud") {
+      window.open(`https://www.icloud.com.cn`);
+    } else {
+      window.open(`https://www.icloud.com.cn/${e.currentTarget.id}`);
+    }
+  });
+});
 
+Array.from(document.getElementsByClassName("hover")).forEach(function (
+  element
+) {
+  element.addEventListener("mouseover", function (e) {
+    document.getElementById(e.currentTarget.id).classList.add("is-highlighted");
+  });
+});
 
-Array.from(document.getElementsByClassName("hover"))
-    .forEach(function(element){
-        element.addEventListener('mouseover', function(e) {
-            document.getElementById(e.currentTarget.id).classList.add("is-highlighted")
-        })
-    });
-
-Array.from(document.getElementsByClassName("hover"))
-    .forEach(function(element){
-        element.addEventListener('mouseout', function(e) {
-            document.getElementById(e.currentTarget.id).classList.remove("is-highlighted")
-        })
-    });
-
-
+Array.from(document.getElementsByClassName("hover")).forEach(function (
+  element
+) {
+  element.addEventListener("mouseout", function (e) {
+    document
+      .getElementById(e.currentTarget.id)
+      .classList.remove("is-highlighted");
+  });
+});
